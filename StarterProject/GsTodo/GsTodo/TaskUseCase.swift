e4814b96ddaf92711e82825b4fad520bbcf03cc8
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-02-07 23:25:39 +0900

    add TaskUseCase ①Firestoreとやりとりするファイルを作成

---
+++ StarterProject/GsTodo/GsTodo/TaskUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  TaskUseCase.swift
+//  GsTodo
+//
+//  Created by Tatsuya Yamamoto on 2020/02/07.
+//  Copyright © 2020 yamamototatsuya. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class TaskUseCase {
+    let db = Firestore.firestore()
+}
