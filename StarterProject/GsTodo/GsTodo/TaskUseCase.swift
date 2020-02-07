6506a0b5db01bd5d54fae652ce3987bd122152ac
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-02-08 01:06:01 +0900

    ②TaskUseCaseに読み込み処理を作成

--- StarterProject/GsTodo/GsTodo/TaskUseCase.swift
+++ StarterProject/GsTodo/GsTodo/TaskUseCase.swift
@@ -61,4 +61,21 @@ class TaskUseCase {
             }
         }
     }
+    
+    func fetchTaskDocuments(callback: @escaping ([Task]?) -> Void){
+        let collectionRef = getCollectionRef()
+        collectionRef.getDocuments(source: .default) { (snapshot, err) in
+            guard err == nil, let snapshot = snapshot,!snapshot.isEmpty else {
+                print("データ取得失敗",err.debugDescription)
+                callback(nil)
+                return
+            }
+            
+            print("データ取得成功")
+            let tasks = snapshot.documents.compactMap { snapshot in
+                return try? Firestore.Decoder().decode(Task.self, from: snapshot.data())
+            }
+            callback(tasks)
+        }
+    }
 }
